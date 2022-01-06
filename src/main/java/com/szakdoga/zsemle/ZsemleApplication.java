4231ef89839df4546793577b73a4bb6ff4e4ce9e
KoyomiOniichan <benedek.bacskay@gmail.com>
2022-01-06 21:10:15 +0100

    Initial setup

---
+++ src/main/java/com/szakdoga/zsemle/ZsemleApplication.java
@@ -0,0 +1,13 @@
+package com.szakdoga.zsemle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsemleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsemleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szakdoga/zsemle/ZsemleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szakdoga.zsemle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZsemleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
